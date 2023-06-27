b4ffefb58af92c13b12a35924305d2a12c5a2439
SergioMayen <sergio.mayen@ct.uneatlantico.es>
2023-06-27 11:51:04 +0200

    fix: getting the body succesfull

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ import { router } from "./routes";
 import db from "./config/mongo";
 
 const PORT = process.env.PORT || 3001;
-const app = express()
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 app.use(router);
-app.use(express.json);
 db().then(() => console.log('Conexion Ready'));
 app.listen(PORT, () => console.log(`Listo para correr perro en el puerto ${PORT}`));
